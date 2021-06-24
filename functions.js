28c9b9fa8f5fb9b5b3d0d6b5623ba25b46b6854a
James Smalls <50281620+jsmalls05@users.noreply.github.com>
2021-06-24 06:02:12 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,31 @@
+//isEven
+
+// function isEven(num){
+// 	if(num % 2 === 0) {
+// 	return true;
+// 	} else {
+// 		return false
+// 	}
+// }
+
+// function isEven(num){
+// 	return num % 2 === 0;
+// }
+
+// function factorial(num){
+// 	//define a result varible
+// 	var result = 1;
+// 	//calculate factorial and store value in result
+// 	for(var i = 2; i <= num; i++){
+// 		result *= i;
+// 	}
+// 	//return the result variable
+// 	return result;
+// }
+
+function kebabToSnake(str) {
+	//replace all '_' with "_"'s 
+	var newStr = str.replace(/-/g , "_");
+	//return str
+	return newStr;
+}
